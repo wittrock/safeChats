71ff91dcc7a188601ce90bb4350c705037aab418
John Wittrock <wittrock.jw@gmail.com>
2012-10-17 22:46:10 -0400

    Forgot to add a file

---
+++ src/BufferPusher.java
@@ -0,0 +1,36 @@
+import java.util.*;
+import java.util.concurrent.LinkedBlockingQueue;
+
+
+public class BufferPusher extends Thread {
+    private List<Chatter> chatters;
+    private LinkedBlockingQueue<String> writeBuffer;
+
+    public BufferPusher(List<Chatter> chatters, LinkedBlockingQueue<String> writeBuffer) {
+	this.chatters = chatters;
+	this.writeBuffer = writeBuffer;
+    }
+
+    public String getMessage() {
+	synchronized(writeBuffer) {
+	    try {
+	    return writeBuffer.take();
+	    } catch (InterruptedException e) {
+		System.out.println("BufferPusher InterruptedException!");
+		e.printStackTrace();
+		return null;
+	    }
+	}
+    }
+
+
+    public void run(){
+	while(true) {
+	    String str = getMessage();
+	    for(Chatter c : chatters) {
+		c.addMessage(str);
+	    }
+	}
+    }
+
+}
